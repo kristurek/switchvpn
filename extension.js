c39b908f82c0af6452406a1632259fef8baffb00
Kris Turek <15099890+kristurek@users.noreply.github.com>
2020-01-13 15:47:11 -0600

    Upgrade to Gnome Shell 3.34.1

--- extension.js
+++ extension.js
@@ -1,174 +1,177 @@
-'use strict'
-
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const Lang = imports.lang;
-
+const GObject = imports.gi.GObject;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const NM = imports.gi.NM;
-const Mainloop = imports.mainloop;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
 
-class NMVpnItem extends PopupMenu.PopupSwitchMenuItem {
+let NMVpnItem = GObject.registerClass(
+    class NMVpnItem extends PopupMenu.PopupSwitchMenuItem {
 
-    constructor(connection) {
-        super(connection.get_id());
+        _init(connection) {
+            super._init(connection.get_id());
 
-        this._connection = connection;
-        this._activeConnection = null;
-    }
+            this._connection = connection;
+            this._activeConnection = null;
+        }
 
-    activate(event) {
-        if (this._switch.actor.mapped)
-            this.toggle();
-    }
+        activate(event) {
+            if (this._switch.mapped)
+                this.toggle();
+        }
 
-    _isActiveConnection() {
-        if (!this._activeConnection)
-            return false;
+        _isActiveConnection() {
+            if (!this._activeConnection)
+                return false;
 
-        return this._activeConnection.state <= NM.ActiveConnectionState.ACTIVATED;
-    }
+            return this._activeConnection.state <= NM.ActiveConnectionState.ACTIVATED;
+        }
 
-    sync(activeConnection) {
-        this._activeConnection = activeConnection;
-        this.setToggleState(this._isActiveConnection());
-    }
+        sync(activeConnection) {
+            this._activeConnection = activeConnection;
+            this.setToggleState(this._isActiveConnection());
+        }
 
-    getActiveConnection() {
-        return this._activeConnection;
-    }
+        getActiveConnection() {
+            return this._activeConnection;
+        }
 
-    getConnection() {
-        return this._connection;
-    }
-}
-
-class PopupMenuVpn extends PanelMenu.Button {
-
-    constructor() {
-        super(1, 'PopupMenuVpn', false);
-
-        let box = new St.BoxLayout();
-        let icon = new St.Icon({
-            icon_name: 'network-vpn-symbolic',
-            style_class: 'system-status-icon'
-        });
-        let toplabel = new St.Label({
-            text: ' VPN ',
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER
-        });
-
-        box.add(icon);
-        box.add(toplabel);
-        box.add(PopupMenu.arrowIcon(St.Side.BOTTOM));
-
-        this.actor.add_child(box);
-
-        this._connectionItems = new Map();
-
-        this._client = NM.Client.new(null);
-        this._client.connect('notify::active-connections', this._sync.bind(this));
-        this._client.connect('connection-added', this._sync.bind(this));
-        this._client.connect('connection-removed', this._sync.bind(this));
-
-        this._sync();
-    }
-
-    _sync() {
-        let currentConnectionItems = new Array(...this._connectionItems).map(entries => entries[1]);
-        let currentConnections = currentConnectionItems.map(x => x.getConnection());
-
-        let newConnections = this._client.get_connections().filter(connection => {
-            return connection.get_setting_by_name(NM.SETTING_CONNECTION_SETTING_NAME).type == NM.SETTING_VPN_SETTING_NAME;
-        });
-
-        let connectionsToUpdate = newConnections.filter(x => currentConnections.includes(x));
-        this._syncConnectionItems(connectionsToUpdate);
-
-        let connectionsToCreate = newConnections.filter(x => !currentConnections.includes(x));
-        this._createConnectionItems(connectionsToCreate);
-
-        let connectionsToRemove = currentConnections.filter(x => !newConnections.includes(x));
-        this._removeConnectionItems(connectionsToRemove);
-    }
-
-    _syncConnectionItems(connections) {
-        if (connections) {
-            let activeConnectionsMap = new Map();
-            let activeConnections = this._client.get_active_connections() || [];
-            activeConnections.forEach((activeConnection) => {
-                activeConnectionsMap.set(activeConnection.get_uuid(), activeConnection);
-            });
-
-            connections.forEach(connection => {
-                let activeConnection = activeConnectionsMap.get(connection.get_uuid());
-                this._connectionItems.get(connection.get_uuid()).sync(activeConnection);
-            });
+        getConnection() {
+            return this._connection;
         }
     }
+)
+let PopupMenuVpn = GObject.registerClass(
+    class PopupMenuVpn extends PanelMenu.Button {
 
-    _createConnectionItems(connections) {
-        if (connections) {
-            connections.forEach(connection => {
-                let nmVpnItem = new NMVpnItem(connection);
-                nmVpnItem.connect('toggled', this._toogle.bind(this));
-                this._connectionItems.set(connection.get_uuid(), nmVpnItem);
-                this.menu.addMenuItem(nmVpnItem);
+        _init() {
+            super._init(1, 'PopupMenuVpn', false);
+
+            let box = new St.BoxLayout();
+            let icon = new St.Icon({
+                icon_name: 'network-vpn-symbolic',
+                style_class: 'system-status-icon'
+            });
+            let toplabel = new St.Label({
+                text: ' VPN ',
+                y_expand: true,
+                y_align: Clutter.ActorAlign.CENTER
             });
 
-            this._syncConnectionItems(connections);
-        }
-    }
+            box.add(icon);
+            box.add(toplabel);
+            box.add(PopupMenu.arrowIcon(St.Side.BOTTOM));
 
-    _removeConnectionItems(connections) {
-        if (connections) {
-            connections.forEach(connection => {
-                this._connectionItems.get(connection.get_uuid()).destroy();
-                this._connectionItems.delete(connection.get_uuid());
+            this.add_child(box);
+
+            this._connectionItems = new Map();
+
+            this._client = NM.Client.new(null);
+
+            this._client.connect('notify::active-connections', this._sync.bind(this));
+            this._client.connect('connection-added', this._sync.bind(this));
+            this._client.connect('connection-removed', this._sync.bind(this));
+            this._sync();
+        }
+
+        _sync() {
+            let currentConnectionItems = new Array(...this._connectionItems).map(entries => entries[1]);
+            let currentConnections = currentConnectionItems.map(x => x.getConnection());
+
+            let newConnections = this._client.get_connections().filter(connection => {
+                return connection.get_setting_by_name(NM.SETTING_CONNECTION_SETTING_NAME).type == NM.SETTING_VPN_SETTING_NAME;
             });
-        }
-    }
 
-    _toogle(object, value) {
-        try {
-            if (value)
-                this._client.activate_connection_async(object.getConnection(), null, null, null, null);
-            else {
-                this._client.deactivate_connection(object.getActiveConnection(), null);
+            let connectionsToUpdate = newConnections.filter(x => currentConnections.includes(x));
+            this._syncConnectionItems(connectionsToUpdate);
+
+            let connectionsToCreate = newConnections.filter(x => !currentConnections.includes(x));
+            this._createConnectionItems(connectionsToCreate);
+
+            let connectionsToRemove = currentConnections.filter(x => !newConnections.includes(x));
+            this._removeConnectionItems(connectionsToRemove);
+        }
+
+        _syncConnectionItems(connections) {
+            if (connections) {
+                let activeConnectionsMap = new Map();
+                let activeConnections = this._client.get_active_connections() || [];
+                activeConnections.forEach((activeConnection) => {
+                    activeConnectionsMap.set(activeConnection.get_uuid(), activeConnection);
+                });
+
+                connections.forEach(connection => {
+                    let activeConnection = activeConnectionsMap.get(connection.get_uuid());
+                    this._connectionItems.get(connection.get_uuid()).sync(activeConnection);
+                });
+            }
+        }
+
+        _createConnectionItems(connections) {
+            if (connections) {
+                connections.forEach(connection => {
+                    let nmVpnItem = new NMVpnItem(connection);
+                    nmVpnItem.connect('toggled', this._toogle.bind(this));
+                    this._connectionItems.set(connection.get_uuid(), nmVpnItem);
+                    this.menu.addMenuItem(nmVpnItem);
+                });
+
+                this._syncConnectionItems(connections);
+            }
+        }
+
+        _removeConnectionItems(connections) {
+            if (connections) {
+                connections.forEach(connection => {
+                    this._connectionItems.get(connection.get_uuid()).destroy();
+                    this._connectionItems.delete(connection.get_uuid());
+                });
+            }
+        }
+
+        _toogle(object, value) {
+            try {
+                if (value)
+                    this._client.activate_connection_async(object.getConnection(), null, null, null, null);
+                else {
+                    this._client.deactivate_connection(object.getActiveConnection(), null);
+                    object.sync(null);
+                }
+            } catch (e) {
+                global.log('Exception [' + e + ']');
                 object.sync(null);
             }
-        } catch (e) {
-            global.log('Exception [' + e + ']');
-            object.sync(null);
+
+            let currentConnectionItems = new Array(...this._connectionItems).map(entries => entries[1]);
+            let currentConnections = currentConnectionItems.map(x => x.getConnection());
+            this._syncConnectionItems(currentConnections)
         }
 
-        let currentConnectionItems = new Array(...this._connectionItems).map(entries => entries[1]);
-        let currentConnections = currentConnectionItems.map(x => x.getConnection());
-        this._syncConnectionItems(currentConnections)
+        destroy() {
+            super.destroy();
+        }
     }
-
-    destroy() {
-        this.parent();
-    }
-}
+)
 
 let app;
 
 function init() {
-
+    log('Initializing ' + Me.metadata.name);
 }
 
 function enable() {
-    app = new PopupMenuVpn();
+    log('Enable ' + Me.metadata.name);
 
-    Main.panel.addToStatusArea('PopupMenuVpn', app, 0, 'right');
+    app = new PopupMenuVpn();
+    Main.panel.addToStatusArea(Me.metadata.name, app, 0, 'right');
 }
 
 function disable() {
+    log('Disable ' + Me.metadata.name);
     app.destroy();
 }
